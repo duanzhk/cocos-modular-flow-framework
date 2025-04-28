3cc8e647336df9bfe5c1f6102a1d43d28d13a425
zhikun.duan <zhikun.duan@ushow.media>
2025-04-28 12:46:31 +0800

    增加自动导出prefab属性功能

--- assets/src/game/gui/UITest.ts
+++ assets/src/game/gui/UITest.ts
@@ -1,56 +1,13 @@
-import { _decorator, Component, Node } from 'cc';
-import { BaseUITest } from '../../views/BaseUITest';
+
+//@ts-ignore
+import { BaseUITest } from 'db://assets/src/views/BaseUITest';
+import { _decorator } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('UITest')
 export class UITest extends BaseUITest {
-    protected onLoad(): void {
-        // console.log('UITest onLoad');
-        // let msg = new Broadcaster()
-        // msg.broadcast("onlogout")
-        // msg
-        // // msg.on('onregister',(data) => {
-        // //     console.log('onlogin', data);
-        // // })
-        // // msg.on({
-        // //     key: 'onlogin',
-        // //     listener: (data) => {
-        // //         console.log('onlogin', data);
-        // //     }
-        // // })
-
-        // msg.on({
-        //     key: 'onlogout',
-        //     listener: (value?: any, callback?: (data?: any, callBack?: any) => void, ...args: any[]) => {
-        //         console.log('onlogout', value);
-        //         if (callback) {
-        //             callback(0, 0);
-        //         }
-        //     }
-        // })
-    }
-    protected start(): void {
-        
-        console.log('UITest start');
-    }
-    protected onDisable(): void {
-        console.log('UITest onDisable');
-    }
-    protected onDestroy(): void {
-        console.log('UITest onDestroy');
-    }
-
-    onEnter(args?: any): void {
-        console.log('UITest onEnter');
-    }
-    onExit(): void {
-        console.log('UITest onExit');
-    }
-    onPause(): void {
-        console.log('UITest onPause');
-    }
-    onResume(): void {
-        console.log('UITest onResume');
-    }
+    onEnter(args?: any): void { }
+    onExit(): void { }
+    onPause(): void { }
+    onResume(): void { }
 }
-
--- assets/src/views/BaseUITest.ts
+++ assets/src/views/BaseUITest.ts
@@ -1,9 +1,11 @@
 
-    import { _decorator, Component, Node } from 'cc';
-    import { BaseView } from "@libs";
-    const { ccclass, property, disallowMultiple } = _decorator;
-    @disallowMultiple()
-    export abstract class BaseUITest extends BaseView {
-        /** @internal */
-        private static readonly __path__: string = "prefabs/common/UITest";
-    }
+import { _decorator,Component,Node,Sprite } from 'cc';
+import { BaseView } from "@libs";
+const { ccclass, property, disallowMultiple } = _decorator;
+@disallowMultiple()
+export abstract class BaseUITest extends BaseView {
+    /** @internal */
+    private static readonly __path__: string = "prefabs/common/UITest";
+    @property({ type: Node }) MyFlag:Node = null!;
+	@property({ type: Sprite }) Line:Sprite = null!;
+}
